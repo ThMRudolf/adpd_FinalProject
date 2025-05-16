04a9ec17764e7c1d83cfb8f59f179b293d32a1aa
ThMRudolf <th.m.rudolf@gmail.com>
2025-05-16 15:21:58 -0600

    first version with dummy values

--- src/py/streamlit_UI.py
+++ src/py/streamlit_UI.py
@@ -12,7 +12,7 @@ df = load_data()
 colH1, colH2 = st.columns(2)
 
 with colH1:
-    st.image("https://cdn.dooca.store/674/products/4-altbier-1.png?v=1594668581&webp=0")
+    st.image("https://github.com/ThMRudolf/adpd_FinalProject/blob/thomas/data/images/beer.png")
 with colH2:
     st.header("BREWSMART")
 st.subheader("BEER RECOMENDATIONS")
@@ -26,10 +26,56 @@ unique_beer_type = df['style'].unique()
 selected_product = st.selectbox("Choose your favorite beer type", unique_beer_type)
 
 
-# Generate a random percentage between 0 and 100
-random_percentage_1 = round(random.uniform(0, 100), 2)
-random_percentage_2 = round(random.uniform(0, 100), 2)
-random_percentage_3 = round(random.uniform(0, 100), 2)
+# Define the stages
+stages = ["Low", "Medium", "High"]
+
+
+col_def1, col_def2, col_def3 = st.columns(3)
+
+with col_def1:
+    # Create the select slider
+    selected_mouthfeel = st.select_slider(
+        "Mouthfeel:",
+        options=stages
+    )
+
+with col_def2:
+    # Create the select slider
+    selected_taste = st.select_slider(
+        "Taste:",
+        options=stages
+    )
+
+with col_def3:
+    # Create the select slider
+    selected_flavor = st.select_slider(
+        "Flavor:",
+        options=stages
+    )
+
+
+# Create the select slider
+selected_aroma = st.select_slider(
+    "Aroma:",
+    options=stages
+)
+
+
+# Some input to evaluate
+#user_input = st.text_input("Find you beer...")
+random_percentage_1 = 0
+random_percentage_2 = 0
+random_percentage_3 = 0
+# Eval button
+if st.button("Find your beer..."):
+    try:
+    # Generate a random percentage between 0 and 100
+        random_percentage_1 = round(random.uniform(0, 100), 2)
+        random_percentage_2 = round(random.uniform(0, 100), 2)
+        random_percentage_3 = round(random.uniform(0, 100), 2)
+    except ValueError:
+        st.error("Ask Water!")
+
 
 
 
