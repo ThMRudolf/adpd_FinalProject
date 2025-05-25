46eb3ed729f5645f59379520afa9fb49b558e4b6
ThMRudolf <th.m.rudolf@gmail.com>
2025-05-25 11:35:08 -0600

    changed utils.py, try to get info from S3, if not get the info from github

--- src/py/utils.py
+++ src/py/utils.py
@@ -8,30 +8,16 @@ from io import BytesIO
 def load_data():
     url = "https://raw.githubusercontent.com/ThMRudolf/adpd_FinalProject/refs/heads/thomas/data/clean/beer_profile_and_ratings.csv"
     df = pd.read_csv(url)
-    url_img = "https://raw.githubusercontent.com/ThMRudolf/adpd_FinalProject/refs/heads/thomas/data/clean/beer_names_breweries_with_images.csv"
-    df_img = pd.read_csv(url_img)
+    try:
+        url_img = "https://itam-analytics-thmrudolf.s3.us-east-1.amazonaws.com/final_project/clean/beer_names_breweries_with_images.csv"
+        df_img = pd.read_csv(url_img)
+    except except Exception as e:
+        print("Access to S3:", e)
+        url_img = "https://raw.githubusercontent.com/ThMRudolf/adpd_FinalProject/refs/heads/thomas/data/clean/beer_names_breweries_with_images.csv"
+        df_img = pd.read_csv(url_img)
     return df, df_img
 
 def resize_image_to_height(url, target_height):
-    """
-    Downloads an image from a given URL and resizes it to the specified height while preserving the aspect ratio.
-    Parameters
-    ----------
-    url : str
-        The URL of the image to be downloaded and resized.
-    target_height : int
-        The desired height (in pixels) for the resized image.
-    Returns
-    -------
-    PIL.Image.Image
-        The resized image as a PIL Image object.
-    Raises
-    ------
-    requests.exceptions.RequestException
-        If there is an issue downloading the image from the URL.
-    PIL.UnidentifiedImageError
-        If the downloaded content is not a valid image.
-    """
     # Load image from URL
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
